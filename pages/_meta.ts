b50a39b3a2e0e9eafd506c9907bc8a3dfa488aa6
Serge Kroes <serge@kroescontrol.nl>
2025-10-01 21:43:47 +0200

    fix: menu folders standaard collapsed

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,13 +1,49 @@
 export default {
   home: "Welkom bij Kroescontrol",
-  "over-kroescontrol": "Over Kroescontrol",
-  cultuur: "Cultuur",
-  "werken-bij": "Werken bij",
-  kennismaking: "Kennismaking",
-  kantoor: "Kantoor",
-  branding: "Branding",
-  klanten: "Klanten",
-  "sna-keurmerk": "SNA Keurmerk",
-  bezoekers: "Voor Bezoekers",
+  "over-kroescontrol": {
+    title: "Over Kroescontrol",
+    type: "menu",
+    items: {}
+  },
+  cultuur: {
+    title: "Cultuur",
+    type: "menu",
+    items: {}
+  },
+  "werken-bij": {
+    title: "Werken bij",
+    type: "menu",
+    items: {}
+  },
+  kennismaking: {
+    title: "Kennismaking",
+    type: "menu",
+    items: {}
+  },
+  kantoor: {
+    title: "Kantoor",
+    type: "menu",
+    items: {}
+  },
+  branding: {
+    title: "Branding",
+    type: "menu",
+    items: {}
+  },
+  klanten: {
+    title: "Klanten",
+    type: "menu",
+    items: {}
+  },
+  "sna-keurmerk": {
+    title: "SNA Keurmerk",
+    type: "menu",
+    items: {}
+  },
+  bezoekers: {
+    title: "Voor Bezoekers",
+    type: "menu",
+    items: {}
+  },
   contact: "Contact"
 }
