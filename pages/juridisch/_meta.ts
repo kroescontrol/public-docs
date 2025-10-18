968782cf25906140c361928ec796dd2a8969508b
Serge Kroes <serge@kroescontrol.nl>
2025-10-18 11:50:40 +0200

    feat: add Notice & Takedown to juridisch menu

--- pages/juridisch/_meta.ts
+++ pages/juridisch/_meta.ts
@@ -2,5 +2,6 @@ export default {
   index: 'Juridisch',
   bedrijfsgegevens: 'Bedrijfsgegevens',
   'sna-keurmerk': 'SNA Keurmerk',
-  privacy: 'Privacy Verklaring'
+  privacy: 'Privacy Verklaring',
+  'notice-and-takedown': 'Notice & Takedown'
 }
